3daf28379199a11cf0f2d2db942667d76cb4b259
Piotr Perzanowski <p.m.perzanowski@gmail.com>
2014-06-01 22:22:38 +0200

    Update NSObject+AutoPropertyInit.h

--- AutoProperty/NSObject+AutoPropertyInit.h
+++ AutoProperty/NSObject+AutoPropertyInit.h
@@ -13,50 +13,6 @@
  `AutoPropertyInit` is an `NSObject` category that simplifies the process of initialize properties that are subclasses of `NSObject`.
  
  `- (void)pp_autoPropertyInit` initializes all object properties for that class. For instance NSString property returns an initialized `NSString` object that contains no characters. `NSArray` initializes a newly allocated array and so on.
- 
- ## Example usage
- 
- Directly in the designated initializer for the class. It is useful for e.g. in data parser, otherwise is better to use lazy loading.
- 
-    #import "AutoInit.h"
-    #import "NSObject+AutoPropertyInit.h"
- 
-    @implementation AutoInit
-    - (instancetype)init {
-        self = [ super init ];
-        if (self) {
-            [self pp_autoPropertyInit];
-        }
-        return self;
-    }
-    @end
- 
- After object creating.
- 
-    @interface MyClass : NSObject
-    @property (readwrite, strong, nonatomic) NSMutableArray *array;
-    @property (readwrite, strong, nonatomic) NSMutableDictionary *dictionary;
-    @end
- 
-    @implementation DoAutoInit
-    - (void)myMethod {
-        MyClass *noAutoInit = [[ MyClass alloc ] init];
-        [noAutoInit pp_autoPropertyInit];
-        noAutoInit.array[0] = @"Some text";
-        noAutoInit.dictionary[@"key"] = @"value";
-    }
-    @end
- 
- ## Contact
- 
- Piotr Perznowski
- 
- - https://github.com/pegan/AutoPropertyInit
- - p.m.perzanowski@gmail.com
- 
- ## License
- 
- NSObject-AutoPropertyInit is available under the BSD 2-Clause license.
  */
 
 #import <Foundation/Foundation.h>
